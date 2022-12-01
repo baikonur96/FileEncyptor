5b3c11f4c4d3a501f4b4cbbb5833eff613ab01e5
Adminsvu <Baikonur96@yandex.ru>
2022-12-01 16:35:47 +0300

    ver.4

--- src/main/java/com/skillbox/java/GUIForme.java
+++ src/main/java/com/skillbox/java/GUIForme.java
@@ -1,13 +1,60 @@
 package com.skillbox.java;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
+import java.io.File;
 
 public class GUIForme {
     private JPanel panel1;
-    private JTextField textField1;
-    private JButton выберитеФайлИлиПапкуButton;
+    private JTextField filePath;
+    private JButton selectButton;
+    private JButton actionButton;
+    private File selectedFile;
 
-    public JPanel getRootPanel(){
+    public GUIForme(){
+        selectButton.addActionListener(new Action(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser = new JFileChooser();
+                chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                chooser.showOpenDialog(panel1);
+                selectedFile = chooser.getSelectedFile();
+                filePath.setText(selectedFile.getAbsolutePath());
+
+            }
+
+            public Object getValue(String key){
+                return null;
+            }
+
+            @Override
+            public void putValue(String key, Object value) {
+
+            }
+
+            @Override
+            public void setEnabled(boolean b) {
+
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return false;
+            }
+
+            @Override
+            public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+            }
+
+            @Override
+            public void removePropertyChangeListener(PropertyChangeListener listener) {
+
+            }
+        });
+    }
+    public JPanel getRootPanel() {
         return panel1;
     }
 }
